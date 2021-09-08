ab3626edcbd564c86e5fce908dec5ad88c930e0d
Ani <aniasatryan336@gmail.com>
2021-09-08 11:22:35 +0400

    Improving animation for case studies

--- src/components/blog-posts/blog-section/BlogSection.js
+++ src/components/blog-posts/blog-section/BlogSection.js
@@ -6,24 +6,34 @@ import { CaseStudiesImages } from "./CaseStudiesImages";
 
 function BlogSection() {
     const {ref, inView} = useInView({
-        threshold: 0.2,
+        threshold: 0.5,
         triggerOnce: false
     });
 
     const animateImages = useAnimation();
+    const animateText = useAnimation();
 
     useEffect(()=> {
         if(inView) {
             animateImages.start({
-                height: 600,
+                height: 280,
                 transition: {
-                    duration: 1.5
+                    duration: 1
+                }
+            })
+            animateText.start({
+                opacity: 1,
+                transition: {
+                    duration: 1
                 }
             })
         }
         if(!inView) {
             animateImages.start({
                 height: 0,
+            })
+            animateText.start({
+                opacity: 0,
             })
         }
     }, [inView])
@@ -32,16 +42,16 @@ function BlogSection() {
         <div className={classes.container} ref={ref}>
             {CaseStudiesImages.map((image)=>{
                 return (
-                    <motion.div className={image.container} animate={animateImages}>
-                        <div className={classes.img_container} >
+                    <div className={image.container} >
+                        <motion.div className={classes.img_container} animate={animateImages}>
                             <img className={classes.img} src={image.src}/>
-                        </div>
-                        <div className={classes.text_container}>
+                        </motion.div>
+                        <motion.div className={classes.text_container} animate={animateText}>
                             <p className={classes.category_title}>{image.subject}</p>
                             <span>{image.title}</span>
                             <p className={classes.text}>{image.text}</p>
-                        </div>
-                    </motion.div>
+                        </motion.div>
+                    </div>
                 )
             })}
         </div>
